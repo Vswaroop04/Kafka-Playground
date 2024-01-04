4792510c723133eee9bc3007dc254232c365079f
vishnuoruphones <vswaroop04@gmail.com>
2024-01-05 00:51:23 +0530

    feat : adding cli interface

--- Consumer.js
+++ Consumer.js
@@ -1,13 +1,14 @@
 const { kafka } = require("./client");
+const group = process.argv[2]
 
 async function init() {
-  const consumer = kafka.consumer({ groupId: "user1" });
+  const consumer = kafka.consumer({ groupId : group});
 
   await consumer.connect();
     await consumer.subscribe({ topics: ["rider-updates"] , fromBeginning : true});
     await consumer.run({
         eachMessage: async ({ topic, partition, message }) => { 
-            console.log(`Received message on ${topic}[${partition}]:`,message);
+            console.log(`Received message on Group : ${group}, Topic[Partition] : ${topic}[${partition}]:`,message.value.toString());
         }
     })
 }
--- Producer.js
+++ Producer.js
@@ -1,5 +1,9 @@
 const { kafka } = require("./client");
+const readline = require("readline");
 
+const rl = readline.createInterface({
+  input: process.stdin,
+});
 async function init() {
   const producer = kafka.producer();
 
@@ -7,23 +11,27 @@ async function init() {
   await producer.connect();
   console.log("Connected to Kafka Server!");
 
-  await producer.send({
-    topic: "rider-updates",
-    messages: [
-      {
-        partition: 0,
-        key: "Location Update",
-        value: JSON.stringify({
-          name: "K V Vishnu Swaroop",
-          location: "Hyderabad",
-        }),
-      },
-    ],
+  rl.setPrompt("> ");
+  rl.prompt();
+  rl.on("line", async function (line) {
+    const [riderName, location] = line.split(" ");
+    await producer.send({
+      topic: "rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "north" ? 0 : 1,
+          key: "Location Update",
+          value: JSON.stringify({
+            riderName,
+            location,
+          }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
+    console.log("Producer Disconnected");
   });
-  console.log("Producer Sent Message ");
-
-//   await producer.disconnect();
-//   console.log("Producer Disconnected");
 }
 
 init();
